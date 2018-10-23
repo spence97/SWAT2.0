74354ed3cb285e49c79479326c35556c75a4dca6
spence97 <spencermcdonald11@gmail.com>
2018-10-23 02:17:41 -0700

    move temp data to app workspace

--- tethysapp-swat2/tethysapp/swat2/config.py
+++ tethysapp-swat2/tethysapp/swat2/config.py
@@ -1,6 +1,9 @@
 import os
+from .app import Swat2
 
-temp_workspace = os.path.join('/home/ubuntu/tethys_temp/swat/')
+
+
+temp_workspace = os.path.join(Swat2.get_app_workspace().path, 'swat')
 
 data_path = os.path.join('/home/ubuntu/swat_data/')
 
@@ -14,7 +17,7 @@ geoserver = {'rest_url':'http://216.218.240.206:8080/geoserver/rest/',
 
 nasaaccess_path = os.path.join('/home/ubuntu/nasaaccess_data')
 
-nasaaccess_temp = os.path.join('/home/ubuntu/tethys_temp/nasaaccess')
+nasaaccess_temp = os.path.join(Swat2.get_app_workspace().path, 'nasaaccess')
 
 nasaaccess_script = os.path.join('/home/ubuntu/subprocesses/nasaaccess.py')
 
