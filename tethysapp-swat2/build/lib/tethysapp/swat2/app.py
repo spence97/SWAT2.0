be46c9da0f0de6ba25d064e2b2a42433413c46bb
spence97 <spencermcdonald11@gmail.com>
2018-11-09 12:19:37 +0700

    fix ol links

---
+++ tethysapp-swat2/build/lib/tethysapp/swat2/app.py
@@ -0,0 +1,92 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+from tethys_sdk.app_settings import PersistentStoreDatabaseSetting
+
+
+class Swat2(TethysAppBase):
+    """
+    Tethys app class for SWAT Data Viewer.
+    """
+
+    name = 'Lower Mekong - SWAT'
+    index = 'swat2:home'
+    icon = 'swat2/images/logo.png'
+    package = 'swat2'
+    root_url = 'swat2'
+    color = '#162d51'
+    description = 'Application to access and analyse inputs and outputs of the Soil and Water Assessment Tool (SWAT)'
+    tags = '&quot;Hydrology&quot;, &quot;Soil&quot;, &quot;Water&quot;, &quot;Timeseries&quot;'
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='swat2',
+                controller='swat2.controllers.home'
+            ),
+            UrlMap(
+                name='update_selectors',
+                url='swat2/update_selectors',
+                controller='swat2.ajax_controllers.update_selectors'
+            ),
+            UrlMap(
+                name='get_upstream',
+                url='swat2/get_upstream',
+                controller='swat2.ajax_controllers.get_upstream'
+            ),
+            UrlMap(
+                name='save_json',
+                url='swat2/save_json',
+                controller='swat2.ajax_controllers.save_json'
+            ),
+            UrlMap(
+                name='timeseries',
+                url='swat2/timeseries',
+                controller='swat2.ajax_controllers.timeseries'
+            ),
+            UrlMap(
+                name='clip_rasters',
+                url='swat2/clip_rasters',
+                controller='swat2.ajax_controllers.clip_rasters'
+            ),
+            UrlMap(
+                name='coverage_compute',
+                url='swat2/coverage_compute',
+                controller='swat2.ajax_controllers.coverage_compute'
+            ),
+            UrlMap(
+                name='run_nasaaccess',
+                url='swat2/run_nasaaccess',
+                controller='swat2.ajax_controllers.run_nasaaccess'
+            ),
+            UrlMap(
+                name='save_file',
+                url='swat2/save_file',
+                controller='swat2.ajax_controllers.save_file'
+            ),
+            UrlMap(
+                name='download_files',
+                url='swat2/download_files',
+                controller='swat2.ajax_controllers.download_files'
+            ),
+        )
+
+        return url_maps
+
+    def persistent_store_settings(self):
+        ps_settings = (
+            PersistentStoreDatabaseSetting(
+                name='swat_db',
+                description='Primary database for SWAT Online app.',
+                initializer='swat2.model.init_db',
+                required=True
+            ),
+        )
+
+        return ps_settings
---
+++ tethysapp-swat2/build/lib/tethysapp/swat2/config.py
@@ -0,0 +1,28 @@
+import os
+from .app import Swat2
+
+temp_workspace = os.path.join(Swat2.get_app_workspace().path, 'swat')
+
+data_path = os.path.join('/home/ubuntu/swat_data/')
+
+geoserver = {'rest_url':'http://216.218.240.206:8080/geoserver/rest/',
+             'wms_url':'http://216.218.240.206:8080/geoserver/wms/',
+             'wfs_url':'http://216.218.240.206:8080/geoserver/wfs/',
+             'user':'admin',
+             'password':'geoserver',
+             'workspace':'swat'
+             }
+
+db = {'name': 'swat_db',
+            'user':'tethys_super',
+            'pass':'pass',
+            'host':'localhost',
+            'port':'5435'}
+
+nasaaccess_path = os.path.join('/home/ubuntu/nasaaccess_data')
+
+nasaaccess_temp = os.path.join(Swat2.get_app_workspace().path, 'nasaaccess')
+
+nasaaccess_script = os.path.join('/home/ubuntu/subprocesses/nasaaccess.py')
+
+nasaaccess_log = os.path.join('/home/ubuntu/subprocesses/nasaaccess.log')
